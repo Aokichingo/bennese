849603ca9aa5b465ce38cb59119d3ba1b198e58f
Aokichingo <aokikatsushige822@gmail.com>
2019-08-14 11:38:08 +0900

    first commit

---
+++ bootstrap/bootstrap.js
@@ -0,0 +1,9 @@
+$(function() {
+  $('body').fadeIn(1200)
+  $('.top ,.author').fadeIn(3000)
+  $('.cm').fadeIn(5000)
+  $('.intro').hover(function(){
+   $(this).find('.w').fadeIn();
+  });
+  
+});
